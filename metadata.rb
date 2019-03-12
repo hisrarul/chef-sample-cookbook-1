14a1ad1cc47a79c10e7d0523bdf0dd4bd4058b09
hisrarul <35391097+hisrarul@users.noreply.github.com>
2019-03-12 20:45:06 +0530

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name 'chef_sample_cookbook_1'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures package_install'
-long_description 'Installs/Configures package_install'
-version '0.1.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+#maintainer 'The Authors'
+#maintainer_email 'you@example.com'
+#license 'All Rights Reserved'
+#description 'Installs/Configures package_install'
+#long_description 'Installs/Configures package_install'
+#version '0.1.0'
+#chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
